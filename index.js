bee6a3965804129a0f52c930ba7e2f55528d92d9
keith <keith.wachira@students.moringaschol.com>
2023-10-03 16:43:15 +0300

    lab done

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+let counter = 0;
+//=> undefined
 
-// Write your code here
+counter++;
+//=> 0
+
+counter;
+//=> 1
+
+let counter = 0;
+//=> undefined
+
+++counter;
+//=> 1
+
+counter;
+//=> 1
+let counter = 0;
+//=> undefined
+
+// Return the current value of 'counter' and then decrement by 1
+counter--;
+//=> 0
+
+// Check the new value of 'counter'
+counter;
+//=> -1
+
+// Decrement 'counter' and then return the new value
+--counter;
+//=> -2
+
+// Check the new value of 'counter'
+counter;
+//=> -2 
